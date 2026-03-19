d9bb3bfbd050bea269e549b6ba7c6c1cc61a7223
hsinyuwww <wen.hsi@northeastern.edu>
2026-03-19 14:21:19 -0400

    Fix TypeScript error

--- app/page.tsx
+++ app/page.tsx
@@ -66,7 +66,7 @@ export default function SkildAIWebsite() {
     setIsAutoPlay(false);
   };
 
-  const goToSlide = (index) => {
+  const goToSlide = (index: number) => {
     setCurrentDemoSlide(index);
     setIsAutoPlay(false);
   };
